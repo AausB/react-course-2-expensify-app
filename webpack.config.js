3b9407c5a6f3a9830d3d8bbd39993a48d871f131
Alexander Mayer <alexander.mayer@struktur1.com>
2018-01-31 16:37:39 +0100

    Fixed firebase setting in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = (env) => {
       new webpack.DefinePlugin({ // JSON.stringify() wraps variable content in quotes ""
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE': JSON.stringify(process.env.FIREBASE_DATABASE),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
         'process.env.FIREBASE_SENDER_ID': JSON.stringify(process.env.FIREBASE_SENDER_ID)
