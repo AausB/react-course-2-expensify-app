467773d3dfcf4932d518bacdf7653401850fd689
Alexander Mayer <alexander.mayer@struktur1.com>
2018-02-04 16:56:32 +0100

    Added comments to component PrivateRoute

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -3,10 +3,11 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 
+// named export for the un-connected component
 export const PrivateRoute = ({
   isAuthenticated,
   component: Component, // rename component to Component
-  ...rest // rest operator: a variable 'rest' for all which was not destructered
+  ...rest // rest parameter syntax: a parameter 'rest' for the rest of the parameters which were not destructered; it's an array of arguments
 }) => (
   <Route {...rest} component={(props) => (
     isAuthenticated ? (
@@ -20,8 +21,10 @@ export const PrivateRoute = ({
   )}/>
 );
 
+// get the authentication uid from the state
 const mapStateToProps = (state) => ({
   isAuthenticated: !!state.auth.uid // turn it into a real boolean value
 });
 
+// default export for the connected component
 export default connect(mapStateToProps)(PrivateRoute);
